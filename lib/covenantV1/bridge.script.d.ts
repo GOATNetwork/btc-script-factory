a5adc0e8e7fe032a5c029aa13cf50ac3753f7c4c
Rygoatei <ryan@zkm.io>
2024-09-01 20:22:12 +0800

    feat: Implement deposit fixed address transaction and data embedding script with parsing (#29)
    * refactor: change calculation
    
    * refactor: optimze fee calculation
    
    * fix: commit build files
    
    * chore: commit audit report file
    
    * feat: fireblocks
    
    * feat: send fixed address for deposit with embed data script with OP_RETURN
    
    * chore: fixed lint issues
    
    * refactor: remove depositerKey from embeding script
    
    * refactor: remove version from embeding script

--- lib/covenantV1/bridge.script.d.ts
+++ lib/covenantV1/bridge.script.d.ts
@@ -7,3 +7,21 @@
  * @return {Buffer} - The compiled script buffer ready for use in blockchain transactions.
  */
 export declare function buildDepositScript(evmAddress: Buffer, posPubkey: Buffer): Buffer;
+/**
+ * Builds a data embedding script.
+ * Assumes little-endian byte order for multi-byte values.
+ * @param {Buffer} magicBytes - Magic bytes, length of 4 bytes.
+ * @param {Buffer} evmAddress - EVM address, length of 20 bytes.
+ * @return {Buffer} Compiled script.
+ */
+export declare function buildDataEmbedScript(magicBytes: Buffer, evmAddress: Buffer): Buffer;
+/**
+ * Parses a data embedding script.
+ * Assumes little-endian byte order for multi-byte values.
+ * @param {Buffer} dataEmbedScript - The data embedding script to parse.
+ * @return {Object} Parsed data including magicBytes and evmAddress.
+ */
+export declare function parseDataEmbedScript(dataEmbedScript: Buffer): {
+    magicBytes: Buffer;
+    evmAddress: Buffer;
+};
