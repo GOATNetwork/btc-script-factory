7a25686284e8c53b659db763880a9e278e25134d
ddx2024 <qiao.m@zkm.io>
2024-11-12 21:18:50 +0800

    feat: bridge support p2wsh input

--- src/covenantV1/bridge.ts
+++ src/covenantV1/bridge.ts
@@ -80,6 +80,12 @@ export function depositTransaction(
     if (input.rawTransaction) {
       newInput.nonWitnessUtxo = Buffer.from(input.rawTransaction, "hex");
     }
+    if (input.witnessScript) {
+      newInput.witnessScript = input.witnessScript;
+    }
+    if (input.sequence) {
+      newInput.sequence = input.sequence;
+    }
     psbt.addInput(newInput);
   });
 
